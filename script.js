bfb124555b333ed5d370a04e57c76068caecbbea
Aishwarya <142329750+A-9514@users.noreply.github.com>
2024-10-09 21:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// your code here
+document.getElementById("button").addEventListener("click", function() {
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+    var baseURL = "https://localhost:8080/";
+
+    if (name && year) {
+        var url = `${baseURL}?name=${name}&year=${year}`;
+        document.getElementById("url").textContent = url;
+    }
+});
